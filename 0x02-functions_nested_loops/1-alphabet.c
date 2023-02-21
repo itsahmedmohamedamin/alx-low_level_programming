e73e0dd924aff5f98480c3c2afccc0db5d93d029
Ahmed Amin <amin14259@gmail.com>
2023-02-21 08:25:23 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/** 
+ * print_alphabet - print lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char alpha;
+alpha = 'a';
+while (alpha <= 'z')
+{
+_putchar(alpha);
+alpha++;
+}
+_putchar('\n');
+}
