f3a6eb0a6da296898abedfdda92ca37f284d6ba5
Ahmed Amin <amin14259@gmail.com>
2023-02-22 13:19:14 -0800

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,7 +12,7 @@ int main(void)
   unsigned long fib1 = 0, fib2 = 1, sum;
   for (count = 0; count < 50; count++)
     {
-sun = fib1 + fib2;
+sum = fib1 + fib2;
 printf("%lu", sum);
 fib1 = fib2;
 fib2 = sum;
