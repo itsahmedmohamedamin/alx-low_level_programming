85916f506dea3d292cd1de311e6494973f6aa8a5
Ahmed Amin <amin14259@gmail.com>
2023-02-27 13:24:53 -0800

    Task 1

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* _strlen - Returns the length of a string.
-* @str: The string to get the length of.
-*
-* Return: The length of @str.
+* _strlen - returns the length of a string
+* @s: string
+* Return: length
 */
+
 int _strlen(char *s)
 {
 int longi = 0;
